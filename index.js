b9d38a301869cb6cd045ba67403830a738d8da43
Anna Pojawis <annapojawis@gmail.com>
2018-11-29 08:26:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function takeANumber(katzDeliLine, name) {
+  for (let i = 0; i<katzDeliLine.length; i++)
+  return `Welcome, ${name}. You are number ${katzDeliLine} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  
+}
